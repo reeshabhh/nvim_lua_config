d36fc99a810a53daf2bd17411743e80f66c78334
Reeshabh Choudhary <reesh678@gmail.com>
2025-01-28 09:46:32 +0530

    Modularize multiple libraries

--- init.lua
+++ init.lua
@@ -1,10 +1,3 @@
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set number relativenumber")
-vim.cmd("set clipboard=unnamedplus")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
   local lazyrepo = "https://github.com/folke/lazy.nvim.git"
@@ -29,18 +22,7 @@ vim.g.maplocalleader = "\\"
 
 
 local opts = {}
+
+require("vim-options")
 require("lazy").setup("plugins")
-local builtin = require("telescope.builtin")
-vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal left<CR>", {})
 
-local config = require("nvim-treesitter.configs") 
-config.setup({
-    ensure_installed = {"python", "lua", "c", "cpp", "rust", "javascript", "vim", "powershell", "markdown", "julia", "kotlin", "go", "json"},
-    highlight = { enable = true },
-    indent = { enable = true }
-})
-
-require("catppuccin").setup()
-vim.cmd.colorscheme "catppuccin"
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,1 +1,12 @@
-return {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+return {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function ()
+        local config = require("nvim-treesitter.configs") 
+        config.setup({
+        ensure_installed = {"python", "lua", "c", "cpp", "rust", "javascript", "vim", "powershell", "markdown", "julia", "kotlin", "go", "json"},
+        highlight = { enable = true },
+        indent = { enable = true },
+    })
+    end
+}

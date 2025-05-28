babe2b6a40e6b3957b8f17559b8174197ab9dfac
Reeshabh Choudhary <reesh678@gmail.com>
2025-05-29 01:02:35 +0530

    Modify lua/plugins/treesitter.lua to automatically install language specific parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,8 @@ return {
     config = function ()
         local config = require("nvim-treesitter.configs") 
         config.setup({
-        ensure_installed = {"python", "lua", "c", "cpp", "rust", "javascript", "vim", "powershell", "markdown", "julia", "kotlin", "go", "json"},
+        -- ensure_installed = {"python", "lua", "c", "cpp", "rust", "javascript", "vim", "powershell", "markdown", "julia", "kotlin", "go", "json"},
+        auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
     })

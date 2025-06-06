34d0b9fc60dd8090294899b2a94f5f429b7703d4
Reeshabh Choudhary <reesh678@gmail.com>
2025-06-07 00:06:49 +0530

    Neovim for Newbs - EP4 - Autocomplete and snippets in Neovim

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,28 +1,29 @@
 return {
-    {    "williamboman/mason.nvim",
-    config = function()
-        require('mason').setup()
-    end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "pyright", "ruff", "rust_analyzer", "ts_ls"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function ()
-            local lspconfig = require("lspconfig")
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            lspconfig.lua_ls.setup({})
-            lspconfig.pyright.setup({})
-            lspconfig.ruff.setup({})
-            lspconfig.rust_analyzer.setup({})
-            lspconfig.ts_ls.setup({})
-        end
-    },
-
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "pyright", "ruff", "rust_analyzer", "ts_ls" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local lspconfig = require("lspconfig")
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			lspconfig.lua_ls.setup({capabilities = capabilities})
+			lspconfig.pyright.setup({capabilities = capabilities})
+			lspconfig.ruff.setup({capabilities = capabilities})
+			lspconfig.rust_analyzer.setup({capabilities = capabilities})
+			lspconfig.ts_ls.setup({capabilities = capabilities})
+		end,
+	},
 }
